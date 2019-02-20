92f4fed0b90c5f493b4acb0582f31253007ca796
Yuxiang Chen <chenyx.cs@gmail.com>
2019-02-20 19:23:57 +0800

    完成评论时邮件通知功能
    * 下面进行服务器的部署
    * 第三方登录和消息通知未做

--- mysite/comment/models.py
+++ mysite/comment/models.py
@@ -1,7 +1,32 @@
+import threading
+
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.conf import settings
+from django.template.loader import render_to_string
+
+
+# 考虑多线程 复杂方案：Celery  可定时执行一些任务
+class Send_Email(threading.Thread):
+    def __init__(self, subject, text, email, fail_silently=False):
+        self.subject = subject
+        self.text = text
+        self.email = email
+        self.fail_silently = fail_silently
+        threading.Thread.__init__(self)
+
+    def run(self):
+        send_mail(self.subject, 
+                  '', 
+                  settings.EMAIL_HOST_USER, 
+                  [self.email], 
+                  fail_silently = self.fail_silently,
+                  html_message = self.text
+        )
+
 
 class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete = models.CASCADE)
@@ -19,6 +44,27 @@ class Comment(models.Model):
     # 用户对象所发出的评论  被哪些评论回复过
     reply_to = models.ForeignKey(User, related_name = "replies", null=True, on_delete=models.CASCADE)
 
+
+    def send_email(self):
+        # 发送邮件通知       
+        if self.parent is None:
+            # 发给博客的作者
+            email = self.content_object.get_email()
+            subject = '有人评论你的博客(#^.^#) from RW\'s Blog' 
+        else:
+            # 发给评论的对象
+            email = self.reply_to.email
+            subject = '有人回复你的评论(#^.^#) from RW\'s Blog'
+
+        if email != "":
+            Dict = {}
+            Dict['comment_text'] = self.text[0:100]
+            Dict['url'] = self.content_object.get_url()+"#comment_id_"+str(self.pk)
+            text = render_to_string('comment/email_tem.html', Dict)
+            # import pdb # 设置断点
+            # pdb.set_trace()
+            send_email = Send_Email(subject, text, email)
+            send_email.start() # 开启线程
 
     def __str__(self):
         return self.text
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -161,4 +161,5 @@ EMAIL_PORT = 25
 EMAIL_HOST_USER = '763972750@qq.com'
 EMAIL_HOST_PASSWORD = 'lgpphcklrmnnbgba'
 EMAIL_SUBJECT_PREFIX = '[RandomWalker\'s Blog]♪(･ω･)ﾉ'
-EMAIL_USE_TLS = True # 与SMTP服务器通信时， 是否启动TLS链接（安全链接）  
+EMAIL_USE_TLS = True # 与SMTP服务器通信时， 是否启动TLS链接（安全链接） 
+EMAIL_COMMENT_SEND = True 
